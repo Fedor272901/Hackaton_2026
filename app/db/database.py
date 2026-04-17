ddc7014ac1ab3dd93e7d42022430af6c8dfdb7cd
FedorGrekov <greckov.fed@yandex.ru>
2026-04-17 12:31:14 +0300

    добавил базовую структуру ORM (только таблицу users с id и name)

---
+++ app/db/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+# загружаем переменные из .env
+load_dotenv()
+
+# берём URL базы
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# создаём подключение к БД
+engine = create_engine(DATABASE_URL)
+
+# фабрика сессий (через неё будем работать с БД)
+SessionLocal = sessionmaker(bind=engine)
+
+# базовый класс для моделей
+Base = declarative_base()
