684640622592c902e99104d7a7071908d878f18d
Nik <nikibutakov@gmail.com>
2026-04-19 01:23:43 +0300

    Подключён новый роутер deputies. подправленны комментарии для четкого понимания

--- app/api_router.py
+++ app/api_router.py
@@ -5,8 +5,10 @@ from app.api.v1.endpoints import (
     category,
     request_status,
     role_request,
-    requests, districts
-    )
+    requests,
+    districts,
+    deputies  # Новый роутер для депутатов
+)
 
 
 
@@ -15,13 +17,14 @@ api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
-#блок 2
+# Блок 2: Обращения (Requests)
 api_router.include_router(requests.router, prefix="/requests", tags=["Requests"])
 
-# блок 3
-api_router.include_router(districts.router, prefix="/districts", tags=["districts"])
+# Блок 3: Округа (Districts) и Депутаты (Deputies)
+api_router.include_router(districts.router, prefix="/districts", tags=["Districts"])
+api_router.include_router(deputies.router, prefix="/deputies", tags=["Deputies"])
 
-# блок 4 — категории, статусы, заявки на роль
+# Блок 4 — категории, статусы, заявки на роль
 api_router.include_router(category.router, prefix="/categories", tags=["Categories"])
 api_router.include_router(request_status.router, prefix="/statuses", tags=["Statuses"])
 api_router.include_router(role_request.router, prefix="/role-requests", tags=["Role Requests"])
